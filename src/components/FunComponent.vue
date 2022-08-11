ff220a12626252691c2e37d76811d41cd594c156
Your Name <you@example.com>
2022-08-11 16:31:27 -0600

    warmup3wk13

---
+++ src/components/FunComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+<button>click me</button>
+  </div>
+</template>
+
+<script>
+  export default {
+   
+   
+   
+   mounted () {
+    
+   },   
+  }
+</script>
+
+<style scoped>
+
+</style>
